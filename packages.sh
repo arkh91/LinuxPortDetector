ac1ff37a772702491ca41cbaaace05bbdf069625
Ali <39136692+arkh91@users.noreply.github.com>
2023-10-17 22:22:01 -0700

    Update packages.sh
    pip package

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 apt-get update  # Update package list
-apt-get install -y net-tools #package2 package3   Replace with actual package names
+apt-get install -y net-tools python3-pip #package2 package3   Replace with actual package names
 pip install pytz
