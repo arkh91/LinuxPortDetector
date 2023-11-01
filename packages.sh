9ef0675a5a79ced0321ff25609d1a195fe70b65d
Ali <39136692+arkh91@users.noreply.github.com>
2023-10-31 18:04:40 -0700

    Update packages.sh
    python script added.

--- packages.sh
+++ packages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 apt-get update  # Update package list
+
+sudo wget https://raw.githubusercontent.com/arkh91/public_script_files/main/python.sh && chmod u+x python.sh && ./python.sh
+echo -e "\033[32mPython script dowanloaded and installed.\033[m"
 apt-get install -y net-tools python3-pip #package2 package3   Replace with actual package names
 pip install pytz
